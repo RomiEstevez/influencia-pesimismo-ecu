65fb852150efa0d27dd1ca2fbd2ff79506f7ac1c
RomiEstevez <romiestevez2002@gmail.com>
2023-08-04 16:28:19 -0500

    Graph conjunto

--- code/pesimismo.R
+++ code/pesimismo.R
@@ -33,6 +33,14 @@ ec_eval_tab <- svyby(formula = ~ ec_eval,
                    na.rm = T,
                    keep.names = F)
 
+econ_sit_tab <- svyby(formula = ~ econ_sit, 
+                      by = ~ year, 
+                      design = dm,
+                      FUN = svymean,
+                      na.rm = T,
+                      keep.names = F)
+
+
 pres_aprov_dic_tab <- svyby(formula = ~ pres_aprov_dic, 
                             by = ~ year, 
                             design = dm,
@@ -49,24 +57,62 @@ theme_article <-
         plot.caption = element_text(color = "grey30", hjust = 0, face = 'italic'),
         legend.background = element_blank())
 
-# Para graficar ec_eval
-graph_sit_pais<-
-  ggplot(ec_eval_tab,
-         aes(x = as.character(year), y = ec_evalWorse, group = 1))+
-  geom_line(size = 1, 
-            color = '#2E5994')+
-  geom_point(size = 2.15,
-             color = '#2E5994')+
+# Para graficar ec_eval y econ_sit
+# Manejo de datos
+sit_econ_pais_df <-
+  ec_eval_tab %>%
+  select(year, 
+         ec_evalWorse, 
+         se.ec_evalWorse) %>% 
+  rename(perc = ec_evalWorse, 
+         se = se.ec_evalWorse) %>% 
+  mutate(legend = 'Situación económica del país')
+
+sit_econ_pers_df <-
+  econ_sit_tab %>%
+  select(year, 
+         econ_sitWorse, 
+         se.econ_sitWorse) %>% 
+  rename(perc = econ_sitWorse, 
+         se = se.econ_sitWorse) %>% 
+  mutate(legend = 'Situación económica personal')
+
+# Juntar ec_eval y econ_sit
+sit_econ_pais_df <-
+  bind_rows(sit_econ_pais_df,
+            sit_econ_pers_df)
+  rownames(sit_econ_pais_df)<-NULL
+
+# Graph pesimismo económico
+graph_sit_econ_df <-
+  ggplot(sit_econ_df,
+         aes(x = as.character(year), y = perc, color = legend, group = legend))+
+  geom_line(size = 1)+
+  scale_color_manual(values = c('#2E5994','#73A5C6'),
+                     breaks = c('Situación económica del país','Situación económica personal'))+
+  geom_point(size = 2.15)+
+  geom_line(aes(x = as.character(year), 
+                y = perc - 1.96*se),
+            size = 0.7,
+            color = 'grey50', 
+            linetype = 'dotted')+
+  geom_line(aes(x = as.character(year), 
+                y = perc + 1.96*se),
+            size = 0.7,
+            color = 'grey50', 
+            linetype = 'dotted')+
   labs(x = '',
        y = '',
-       title = 'Porcentaje que opina que la economía del país es peor\nque hace doce meses') +
+       title = 'Porcentaje que opina que la situación económica del\npaís o su situación personal es peor que hace doce meses') +
   theme_article +
-  theme(plot.title = element_text(size = 14)) +
+  theme(plot.title = element_text(size = 14),
+        legend.title = element_blank(),
+        legend.position = c(0.7,0.9)) +
   scale_y_continuous(limits = c(0.1, 1),
                      breaks = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1),
                      labels = c('10', '20', '30', '40', '50', '60', '70', '80', '90', '100'))
 
-  ggsave("figures/grafico_situacion_pais.png",plot = graph_sit_pais, 
+  ggsave("figures/grafico_pesimismo_econ.png",plot = graph_sit_econ_df, 
          device = "png", 
          width = 10, 
          height = 6, 
@@ -77,11 +123,21 @@ graph_pres_aprov_dic<-
   ggplot(pres_aprov_dic_tab,
            aes(x = as.character(year), y = pres_aprov_dicNo, group = 1))+
   geom_line(size = 1, 
-            color = '#2E5994')+
+            color = '#02826b')+
   geom_point(size = 2.15,
-             color = '#2E5994')+
+             color = '#02826b')+
+  geom_line(aes(x = as.character(year), 
+                y = pres_aprov_dicNo - 1.96*se.pres_aprov_dicNo),
+            size = 0.7,
+            color = 'grey50', 
+            linetype = 'dotted')+
+  geom_line(aes(x = as.character(year), 
+                y = pres_aprov_dicNo + 1.96*se.pres_aprov_dicNo),
+            size = 0.7,
+            color = 'grey50', 
+            linetype = 'dotted')+
   geom_vline(xintercept = 7.5, color = '#5C7C94', linetype = 'dotted')+
-  annotate('label', x = 5, y = 0.4, label = 'Gob. de Correa')+
+  annotate('label', x = 5, y = 0.7, label = 'Gob. de Correa')+
   geom_vline(xintercept = 2.5, color = '#5C7C94', linetype = 'dotted')+
   annotate('label', x = 1.48, y = 0.5, label = 'Gob. de\nGutiérrez-Palacio')+
   annotate('label', x = 8.05, y = 0.5, label = 'Gob. de\nMoreno')+
@@ -102,10 +158,10 @@ ggsave("figures/grafico_desaprobacion_presidente.png",plot = graph_pres_aprov_di
 
 # Graph en conjunto
 caption_graph_conjunto <-
-  'Fuente: El Barómetro de las Américas por el Proyecto de Opinión Pública de América Latina (LAPOP), www.LapopSurveys.org. El % que se ve pesimista ante la situación económica del país se calcula para quienes consideran que el escenario actual es peor que hace 12 meses. El % que se ve pesimista ante el trabajo del Ejecutivo se calcula para quienes consideran que el trabajo del Presidente de turno es regular, malo o pésimo.'
+  'Fuente: El Barómetro de las Américas por el Proyecto de Opinión Pública de América Latina (LAPOP), www.LapopSurveys.org. El % que se ve pesimista ante la situación económica del país se calcula para quienes consideran que el escenario actual es peor que hace 12 meses. El % que se ve pesimista ante su situación económica se calcula para quienes consideran que su economía actual es peor que hace 12 meses. El % que se ve pesimista ante el trabajo del Ejecutivo se calcula para quienes consideran que el trabajo del Presidente de turno es regular, malo o pésimo. Las líneas punteadas en gris representan los límites inferiores y superiores del intervalo de confianza al 95%.'
 
 graph_conjunto <-
-  graph_sit_pais + graph_pres_aprov_dic + 
+  graph_sit_econ_df + graph_pres_aprov_dic + 
   plot_layout(ncol = 2) +
   plot_annotation(title = 'Pesimismo económico y político de los ecuatorianos',
                   caption = str_wrap(caption_graph_conjunto, 210),
